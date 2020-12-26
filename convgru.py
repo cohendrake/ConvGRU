a9acac0c6186627f8d12cc335f8a873e249acf4a
tianyi.zhu <tianyi.zhu@horizon.ai>
2020-12-26 18:10:55 +0800

    Optimize via jit

--- convgru.py
+++ convgru.py
@@ -1,12 +1,14 @@
 import torch
-from torch import nn
+from torch import nn, jit
+import torch.nn.functional as F
+import math
 
 
-class Conv1dGRUCell(nn.Module):
+class Conv1dGRUCell(jit.ScriptModule):
     def __init__(self, dim_in, in_channels, out_channels,
-                 kernel_size, bias):
+                 kernel_size):
         """
-        :param input_size: int
+        :param dim_in: int
             Dimension of input tensor.
         :param in_channels: int
             Number of channels of input tensor.
@@ -14,41 +16,31 @@ class Conv1dGRUCell(nn.Module):
             Number of channels of output tensor as well as hidden state.
         :param kernel_size: int
             Size of the convolutional kernel.
-        :param stride: int
-            Size of the convolutional stride.
-        :param padding: int
-            Size of the convolutional padding.
-        :param bias: bool
-            Whether or not to include the bias.
-        :param dtype: torch.cuda.FloatTensor or torch.FloatTensor
-            Whether or not to use cuda.
         """
         super(Conv1dGRUCell, self).__init__()
         self.dim_in = dim_in
         self.padding = kernel_size // 2
         self.out_channels = out_channels
-        self.bias = bias
 
-        self.i2h_conv = nn.Conv1d(
-                    in_channels,
-                    out_channels*3,
-                    kernel_size,
-                    1,
-                    self.padding,
-                    bias=bias)
+        fan_in_i2h = in_channels * kernel_size
+        fan_out_i2h = 3 * out_channels * kernel_size
+        fan_in_h2h = out_channels * kernel_size
+        fan_out_h2h = fan_out_i2h
+        a_i2h = math.sqrt(6 / (fan_in_i2h+fan_out_i2h))
+        a_h2h = math.sqrt(6 / (fan_in_h2h+fan_out_h2h))
 
-        self.h2h_conv = nn.Conv1d(
-                    out_channels,
-                    out_channels*3,
-                    kernel_size,
-                    1,
-                    self.padding,
-                    bias=bias)
+        self.conv1d_i2h_weight = nn.Parameter(
+            torch.empty(3*out_channels,
+                        in_channels,
+                        kernel_size).uniform_(-a_i2h, a_i2h))
+        self.conv1d_i2h_bias = nn.Parameter(torch.zeros(3*self.out_channels))
+        self.conv1d_h2h_weight = nn.Parameter(
+            torch.empty(3*out_channels,
+                        out_channels,
+                        kernel_size).uniform_(-a_h2h, a_h2h))
+        self.conv1d_h2h_bias = nn.Parameter(torch.zeros(3*self.out_channels))
 
-    def init_hidden(self, batch_size, device):
-        return torch.zeros(batch_size, self.out_channels,
-                           self.dim_in, device=device)
-
+    @jit.script_method
     def forward(self, x, h_prev):
         """
         :param self:
@@ -59,21 +51,32 @@ class Conv1dGRUCell(nn.Module):
         :return h_next: (N, C_hidden, W)
             tensor of hidden state of next step
         """
-        rzn_i = self.i2h_conv(x)
-        rzn_h = self.h2h_conv(h_prev)
+        rzn_i = F.conv1d(input=x,
+                         weight=self.conv1d_i2h_weight,
+                         bias=self.conv1d_i2h_bias,
+                         padding=self.padding)
 
-        ri, zi, ni = torch.split(rzn_i, self.out_channels, dim=1)
-        rh, zh, nh = torch.split(rzn_h, self.out_channels, dim=1)
+        rzn_h = F.conv1d(input=h_prev,
+                         weight=self.conv1d_h2h_weight,
+                         bias=self.conv1d_h2h_bias,
+                         padding=self.padding)
 
-        n = torch.tanh(ni + torch.sigmoid(ri + rh) * nh)
-        z = torch.sigmoid(zi + zh)
+        ri, zi, ni = rzn_i.chunk(3, 1)
+        rh, zh, nh = rzn_h.chunk(3, 1)
+
+        r = ri + rh
+        z = zi + zh
+        r = torch.sigmoid(r)
+        z = torch.sigmoid(z)
+        n = torch.sigmoid(ni + r * nh)
+
         h_next = (1 - z) * n + z * h_prev
         return h_next
 
 
-class Conv1dGRU(nn.Module):
+class Conv1dGRULayer(jit.ScriptModule):
     def __init__(self, dim_in, in_channels, out_channels,
-                 kernel_size, num_layers, bias):
+                 kernel_size):
         """
         :param dim_in: int
             Dimension of input tensor.
@@ -83,88 +86,49 @@ class Conv1dGRU(nn.Module):
             Number of channels of output tensor as well as hidden state.
         :param kernel_size: int
             Size of convolutional kernel.
-        :param num_layers: int
-            Number of ConvGRU layers.
-        :param bias: bool
-            Whether or not to add the bias.
         """
+        super(Conv1dGRULayer, self).__init__()
+        self.cell = Conv1dGRUCell(dim_in, in_channels,
+                                  out_channels, kernel_size)
+
+    @jit.script_method
+    def forward(self, input, h):
+        """
+        :param input: (T, N, Cin, W)
+            input tensor.
+        :param hidden_state: (num_layers, N, C_hidden, W)
+            tensor of hidden state of previous step.
+        :return output: (T, N, Cout, W)
+            output tensor.
+        """
+        inputs = input.unbind(0)
+        outputs = torch.jit.annotate(List[Tensor], [])
+        for i in range(len(inputs)):
+            h = self.cell(inputs[i], h)
+            outputs += [h]
+
+        return torch.stack(outputs, 0)
+
+
+class Conv1dGRU(nn.Module):
+    def __init__(self, dim_in, in_channels, out_channels,
+                 kernel_size, num_layers):
         super(Conv1dGRU, self).__init__()
 
         if isinstance(out_channels, int):
-            out_channels_list = [out_channels] * num_layers
+            cout_list = [out_channels] * num_layers
         else:
-            assert isinstance(out_channels, list) \
-                and len(out_channels) == num_layers
-            out_channels_list = out_channels
-        in_channels_list = [in_channels] + out_channels_list[:-1]
-        if not len(in_channels_list) == num_layers:
-            raise ValueError("Inconsistent list length.")
+            raise NotImplementedError
 
-        cell_list = [Conv1dGRUCell(dim_in,
-                                   cin,
-                                   cout,
-                                   kernel_size,
-                                   bias)
-                     for cin, cout in zip(in_channels_list, out_channels_list)]
-        self.cell_list = nn.ModuleList(cell_list)
-        self.num_layers = num_layers
+        cin_list = [in_channels] + cout_list[:-1]
+        layer_list = [Conv1dGRULayer(dim_in, cin, cout, kernel_size)
+                      for cin, cout in zip(cin_list, cout_list)]
+        self.layer_list = nn.ModuleList(layer_list)
 
-    def forward(self, input_tensor):
-        """
-        :param input_tensor: (N, Cin, W, T)
-            input tensor.
-        :param hidden_state: (N, C_hidden, W, num_layers)
-            tensor of hidden state of previous step.
-        :return output_tensor: (N, Cout, W, T)
-            output tensor.
-        """
+    def forward(self, input, h):
+        hs = h.unbind(0)
+        for i in range(len(hs)):
+            output = self.layer_list[i](input, hs[i])
+            input = output
 
-        hidden_state_list = self._init_hidden(input_tensor.size(1),
-                                              input_tensor.device)
-        seq_len = input_tensor.size(0)
-
-        input_layer_list = torch.unbind(input_tensor, dim=0)
-        for i in range(self.num_layers):
-            h = hidden_state_list[i]
-            output_layer_list = []
-            for t in range(seq_len):
-                h = self.cell_list[i](input_layer_list[t], h)
-                output_layer_list.append(h)
-            input_layer_list = output_layer_list
-        output_tensor = torch.stack(output_layer_list, dim=0)
-
-        '''
-        h_in_list = hidden_state_list
-        output_list = []
-        for t in range(seq_len):
-            input = input_tensor[t, :, :, :]
-            h_out_list = []
-            for i in range(self.num_layers):
-                output = self.cell_list[i](input, h_in_list[i])
-                input = output
-                h_out_list.append(output)
-            h_in_list = h_out_list
-            output_list.append(output)
-        output_tensor = torch.stack(output_list, dim=0)
-        '''
-
-        return output_tensor
-
-    def _init_hidden(self, batch_size, device):
-        init_states_list = [c.init_hidden(batch_size, device)
-                            for c in self.cell_list]
-        return init_states_list
-
-    def initialize(self):
-        for name, param in self.named_parameters():
-            print(name, param.shape)
-            if 'i2h' in name:
-                if 'weight' in name:
-                    nn.init.xavier_uniform_(param)
-                if 'bias' in name:
-                    nn.init.zeros_(param)
-            if 'h2h' in name:
-                if 'weight' in name:
-                    nn.init.orthogonal_(param)
-                if 'bias' in name:
-                    nn.init.zeros_(param)
+        return output
--- test.py
+++ test.py
@@ -3,9 +3,10 @@ import time
 import torch
 from torch import nn
 import numpy as np
-from convgru import Conv1dGRU
+from convgru import Conv1dGRULayer, Conv1dGRUCell, Conv1dGRU
 
 if __name__ == "__main__":
+    '''
     os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
     torch.backends.cudnn.benchmark = True
     batch_size = 16
@@ -48,3 +49,47 @@ if __name__ == "__main__":
         opt.step()
     end = time.time()
     print("Cost {:.2f}".format(end-start))
+    '''
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
+    torch.backends.cudnn.benchmark = True
+    batch_size = 16
+    dim_in = 32
+    T = 635
+    in_channels = 8
+    out_channels = 8
+    kernel_size = 7
+    num_layers = 2
+
+    input_tensor = torch.tensor(np.random.randn(
+        T, batch_size, in_channels, dim_in
+    )).float().cuda()
+
+    label_tensor = torch.tensor(np.random.randn(
+        T, batch_size, in_channels, dim_in
+    )).float().cuda()
+
+    state_tensor = torch.zeros(num_layers, batch_size,
+                               out_channels, dim_in).float().cuda()
+
+    device_ids = range(torch.cuda.device_count())
+    conv1d_gru = Conv1dGRU(dim_in, in_channels, out_channels, kernel_size,
+                           num_layers).float().cuda()
+    conv1d_gru = torch.nn.DataParallel(conv1d_gru, device_ids, dim=1)
+
+    opt = torch.optim.Adam(conv1d_gru.parameters(), 1e-3)
+    criterion = nn.MSELoss(reduction="mean")
+
+    start = time.time()
+    for i in range(10):
+        output_tensor = conv1d_gru(input_tensor, state_tensor)   
+        loss = criterion(output_tensor, label_tensor)
+        print(loss)
+
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+    end = time.time()
+    print(output_tensor.shape)
+    print("Cost {:.2f}".format(end-start))
+
+    # print(cell.graph_for(input_tensor, state_tensor))
