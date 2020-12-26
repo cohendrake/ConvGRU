471e27e3ceb89cb515f65802daeff890591b92c8
tianyi.zhu <tianyi.zhu@horizon.ai>
2020-12-26 18:53:06 +0800

    delete unnecessary codes

--- test.py
+++ test.py
@@ -6,50 +6,6 @@ import numpy as np
 from convgru import Conv1dGRULayer, Conv1dGRUCell, Conv1dGRU
 
 if __name__ == "__main__":
-    '''
-    os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
-    torch.backends.cudnn.benchmark = True
-    batch_size = 16
-    dim_in = 32
-    T = 635
-    in_channels = 8
-    out_channels = 8
-    kernel_size = 7
-    num_layers = 2
-    bias = True
-
-    input_tensor = torch.tensor(np.random.randn(
-                                    T, batch_size, in_channels,
-                                    dim_in).astype(np.float32)).cuda()
-    label = torch.tensor(np.random.randn(
-                                    T, batch_size, in_channels,
-                                    dim_in).astype(np.float32)).cuda()
-
-    # init test
-    device_ids = range(torch.cuda.device_count())
-    conv1d_gru = Conv1dGRU(dim_in, in_channels, out_channels,
-                           kernel_size, num_layers, bias).float()
-    conv1d_gru.initialize()
-    conv1d_gru = conv1d_gru.cuda()
-    conv1d_gru = torch.nn.DataParallel(conv1d_gru, device_ids)
-
-    opt = torch.optim.Adam(conv1d_gru.parameters(), 1e-3)
-    criterion = nn.MSELoss(reduction="mean")
-
-    # forward test
-    start = time.time()
-    for i in range(10):
-        output_tensor = conv1d_gru(input_tensor)
-        loss = criterion(output_tensor, label)
-        print(loss)
-
-        # backward test
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
-    end = time.time()
-    print("Cost {:.2f}".format(end-start))
-    '''
     os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
     torch.backends.cudnn.benchmark = True
     batch_size = 16
